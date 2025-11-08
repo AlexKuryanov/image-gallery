f37127b27731baae5fd4d65646722acd383c7362
AlexKuryanov <lexxus07@yandex.ru>
2025-11-08 19:54:13 +0100

    fix: fix the query to load random photos by first loading

--- image-gallery/main.js
+++ image-gallery/main.js
@@ -1,16 +1,17 @@
-const searchForm = document.querySelector(".search-form");
-const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-btn");
-const galleryWrapper = document.querySelector(".gallery-wrapper");
-const baseUrl = "https://api.unsplash.com/search/photos?page=1&per_page=9";
-let query = "&query=''";
-const clientID = "&client_id=tl5SPOXDUnmZktEBhWhfAJFaQgwYwBZHCKf6jOPqlxE";
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+const galleryWrapper = document.querySelector('.gallery-wrapper');
+const baseUrl = 'https://api.unsplash.com/search/photos?page=1&per_page=9';
+let query = "&query='random'";
+const clientID = '&client_id=tl5SPOXDUnmZktEBhWhfAJFaQgwYwBZHCKf6jOPqlxE';
 let url = `${baseUrl}${query}${clientID}`;
-window.addEventListener("load", getData);
-searchBtn.addEventListener("click", () => {
+window.addEventListener('load', getData);
+
+searchBtn.addEventListener('click', () => {
   query = `&query='${searchInput.value}'`;
   url = baseUrl + query + clientID;
-  galleryWrapper.innerHTML = "";
+  galleryWrapper.innerHTML = '';
   getData();
 });
 
@@ -22,19 +23,19 @@ async function getData() {
 
 function showData(data) {
   data.results.map((result) => {
-    const img = document.createElement("img");
-    img.classList.add("gallery-img");
+    const img = document.createElement('img');
+    img.classList.add('gallery-img');
     img.src = `${result.urls.regular}`;
     img.alt = `${result.alt_description}`;
     galleryWrapper.append(img);
   });
 }
 
-searchForm.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+searchForm.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
     query = `&query='${searchInput.value}'`;
     url = baseUrl + query + clientID;
-    galleryWrapper.innerHTML = "";
+    galleryWrapper.innerHTML = '';
     getData();
   }
 });
